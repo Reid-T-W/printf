8bd240760c6b92f27367fd5a09274331d5227cb2
Reid-T-W <rediettadesse100@gmail.com>
2022-04-17 13:29:08 -0700

    Additional corrections to the select command function

--- main.h
+++ main.h
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
-int select_command(const char *format, va_list list, int i, int count);
+void select_command(const char *format, va_list list, int *i, int *count);
 int _putchar(char c);
 int _prints(char *s);
 int _printint(int i);
