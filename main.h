4fbd0fc96a48c4f7d4b63a53fe0de47d8f2fc3ea
GadisaAsfaw <gadisasfaw@gmail.com>
2022-04-17 10:01:58 -0400

    function prototypes

---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef PRINTF_FUNC
+#define PRINTF_FUNC
+
+int _printf(const char *format, ...);
+void _putchar(char c);
+void _prints(char *s);
+void _printint(int i);void _print_num(int n);
+#endif
