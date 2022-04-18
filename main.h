da5d75d7aa8110066e2fed932a9ea257b13f4a4b
GadisaAsfaw <gadisasfaw@gmail.com>
2022-04-18 16:13:44 -0400

    refactor

--- main.h
+++ main.h
@@ -1,11 +1,19 @@
 #ifndef PRINTF_FUNC
 #define PRINTF_FUNC
+
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+typedef struct print_func
+{
+	char print_ft;
+	int (* p_func)(va_list l);
+} pr_f;
 
 int _printf(const char *format, ...);
 void select_command(const char *format, va_list list, int *i, int *count);
 int _putchar(char c);
-int _prints(char *s);
-int _printint(int i);
-int _print_num(int n);
+int _printc(va_list l);
+int _prints(va_list l);
+int _print_num(va_list l);
 #endif
