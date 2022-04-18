54888696be9ecc1c26e260920d664b8085bdb245
Reid-T-W <rediettadesse100@gmail.com>
2022-04-18 04:41:37 -0700

    Print %

--- _printf.c
+++ _printf.c
@@ -61,6 +61,7 @@ void select_command(const char *format, va_list list, int *i, int *count,
 			if (character != 0)
 				*count += _putchar(character);
 			else
+				_putchar('%');
 				*error = 1;
 			*i += 2;
 			break;
@@ -69,6 +70,7 @@ void select_command(const char *format, va_list list, int *i, int *count,
 			if (string != NULL)
 				*count += _prints(string);
 			else
+				_putchar('%');
 				*error = 1;
 			*i += 2;
 			break;
