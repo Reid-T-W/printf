4802aaad9c790a2767e53ef1c5662dd9ed8ca500
Reid-T-W <rediettadesse100@gmail.com>
2022-04-18 01:08:41 -0700

    print % berfor exit

--- _printf.c
+++ _printf.c
@@ -57,6 +57,7 @@ void select_command(const char *format, va_list list, int *i, int *count)
 			*i += 2;
 			break;
 		case ' ':
+			*count += _putchar('%');
 			exit(EXIT_FAILURE);
 			break;
 		default:
