ad317e7edd8e1cfe9454db54f06ce3d3da291af0
Reid-T-W <rediettadesse100@gmail.com>
2022-04-19 11:45:56 -0700

    _putchar.c made empty

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,1 @@
-#include "main.h"
-#include <stdio.h>
-#include <unistd.h>
-/**
- *_putchar - prints a char
- *@c: The char to be printed
- *Return: void
- */
-int _putchar(char c)
-{
-	write(1, &c, 1);
-	return (1);
-}
+
