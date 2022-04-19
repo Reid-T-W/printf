8c8381a5e75b1be2ac44db65e8c14741af2eaa02
GadisaAsfaw <gadisasfaw@gmail.com>
2022-04-19 11:08:22 -0400

    new prototype added

--- main.h
+++ main.h
@@ -5,21 +5,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
  *struct print_func - struct to select specific print function
  *@print_ft: format specifier
  *@p_func: pinter to function
  */
 typedef struct print_func
 {
-	char print_ft;
-	int (*p_func)(va_list l);
+	char ft;
+	int (*p_func)(va_list l, flags_t *f);
 } pr_f;
 
+int (*get_print_func(char fc))(va_list l,flags_t *);
+int get_flag(char s, flags_t *f);
 int _printf(const char *format, ...);
-void select_command(const char *format, va_list list, int *i, int *count,
-		int *error);
+
 int _putchar(char c);
-int _printc(va_list l);
-int _prints(va_list l);
-int _print_num(va_list l);
+int _puts(char *str);
+void print_number(int n);
+int  count_digit(int i);
+
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+int print_int(va_list l, flags_t *f);
 #endif
